5b57b28d40c6236e1cc88134aeac2ed4e37beaa6
Yuvraj Rathva <rathva.1@iitj.ac.in>
2024-06-03 22:27:10 +0530

    Scraping top 5 trending topics done

---
+++ main.py
@@ -0,0 +1,94 @@
+from selenium import webdriver
+from time import sleep
+import os
+from dotenv import load_dotenv
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+LOGIN_PAGE = "https://x.com/i/flow/login"
+pac_file_url = "https://proxymesh.com/pac/open_us-tx.pac" # Proxy PAC file URL
+
+class TwitterScrapper:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+        self.options = webdriver.ChromeOptions()
+        self.options.add_argument(f'--proxy-pac-url={pac_file_url}') # set proxy
+        # self.options.add_argument("--headless") # to run without a gui
+        self.options.add_argument("start-maximized") # open Browser in maximized mode
+        self.options.add_argument("--disable-blink-features") # disables all Blink (rendering engine) features in the browser
+        self.options.add_argument("--disable-blink-features=AutomationControlled") # disables the AutomationControlled feature in Blink (often used to detect and block automated bots)
+
+        self.service = Service()
+
+        self.driver = self.new_driver()
+        self.driver.maximize_window()
+
+        self.LOGIN_URL = LOGIN_PAGE
+        self.trending_topics = []
+
+
+    def new_driver(self):
+        driver = webdriver.Chrome(service=self.service, options=self.options)
+        return driver
+
+    def login(self, driver):
+        driver.get(self.LOGIN_URL)
+
+        print("Title: " + driver.title)
+
+        driver.implicitly_wait(10)
+
+        username_input = driver.find_element(by = By.XPATH, value ='//input[@autocomplete="username"]')
+        username_input.send_keys(self.username)
+        
+
+        next_button = driver.find_element(by = By.XPATH, value = '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/button[2]')
+        next_button.click()
+
+        driver.implicitly_wait(10)
+
+        password_input = driver.find_element(by = By.XPATH, value = '//input[@autocomplete="current-password"]')
+        password_input.send_keys(self.password)
+
+        login_button = driver.find_element(by = By.XPATH, value = '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/button')
+        login_button.click()
+    
+    def get_driver(self):
+        return self.driver
+    
+    def scrape_trending(self, driver):
+        driver.implicitly_wait(10)
+
+        what_is_happening = driver.find_element(by = By.XPATH, value = '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[2]/div/div[2]/div/div/div/div[4]/div/section/div/div')
+
+        # loop from 3rd div to 8th div and get the text
+        for i in range(3, 8):
+            # only get the text of the trending topic from second span tags (as there are many divs in the div before span tag)
+            trending_topic = what_is_happening.find_element(by = By.XPATH, value = f'//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[2]/div/div[2]/div/div/div/div[4]/div/section/div/div/div[{i}]/div/div/div/div[2]').text
+            self.trending_topics.append(trending_topic)
+
+    def close(self):
+        self.driver.quit()
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    username = os.getenv("USERNAME")
+    password = os.getenv("PASSWORD")
+
+    twitterScrapper = TwitterScrapper(
+        username=username,
+        password=password
+    )
+
+    driver = twitterScrapper.get_driver()
+    twitterScrapper.login(driver)
+    sleep(10)
+
+    twitterScrapper.scrape_trending(driver)
+    sleep(10)
+
+
+    twitterScrapper.close()
